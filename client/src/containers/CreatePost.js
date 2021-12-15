0dd41fb470142b8d9fa9ddaafeadcd26b57f387c
Vishnu Ranjith <vishnu.ranjith@somaiya.edu>
2021-12-15 20:44:37 +0530

    first changes

--- client/src/containers/CreatePost.js
+++ client/src/containers/CreatePost.js
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import { createPost } from '../actions/postsActions';
+import MenuItem from '@material-ui/core/MenuItem';
+import colors from '../misc/colors';
 
 const styles = theme => ({
   button: {
@@ -44,6 +46,7 @@ export class CreatePost extends Component {
   };
 
   render() {
+    const topic = this.state;
     const { postText } = this.state;
     const { classes } = this.props;
     return (
@@ -54,8 +57,25 @@ export class CreatePost extends Component {
         onSubmit={this.handleSubmit}
       >
         <TextField
+          fullWidth
+          select
+          className={classes.textField}
+          value={topic}//create const topic
+          id="topic"
+          label="What do you want to teach today?"
+          margin="normal"
+          name="topic"
+          onChange={this.handleChange}
+        >
+          {colors.map(color => (//repalce color with skill list
+            <MenuItem key={color.value} value={color.value}>
+              {color.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
           id="textarea"
-          placeholder="What's on your mind?"
+          placeholder="Add some details."
           multiline
           className={classes.textField}
           margin="normal"
---
+++ client/src/misc/skills.js
@@ -0,0 +1,36 @@
+const skills = [
+    {
+      value: 0,
+      label: 'Guitar'
+    },
+    {
+      value: 1,
+      label: 'Piano'
+    },
+    {
+      value: 2,
+      label: 'Painting'
+    },
+    {
+      value: 3,
+      label: 'Karate'
+    },
+    {
+      value: 4,
+      label: 'Python'
+    },
+    {
+      value: 5,
+      label: 'Java'
+    },
+    {
+      value: 6,
+      label: 'Violin'
+    },
+    {
+      value: 7,
+      label: 'Carpentry'
+    }
+  ];
+  
+  export default skills;
